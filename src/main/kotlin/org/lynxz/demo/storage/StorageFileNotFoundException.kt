5e1b747ff406deadb606920fa23247d733dda1c0
lynxz <lynxz8866@gmail.com>
2017-12-22 16:56:16 +0800

    添加上传下载文件demo

---
+++ src/main/kotlin/org/lynxz/demo/storage/StorageFileNotFoundException.kt
@@ -0,0 +1,8 @@
+package org.lynxz.demo.storage
+
+class StorageFileNotFoundException : StorageException {
+
+    constructor(message: String) : super(message)
+
+    constructor(message: String, cause: Throwable) : super(message, cause)
+}
